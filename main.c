a76d33e5d6f2088330b0e7eef23e973dff8cb1cc
bhagwat-99 <shindebhagavat030@gmail.com>
2022-03-10 13:32:25 +0530

    read register

--- main.c
+++ main.c
@@ -30,22 +30,22 @@ printf("%x\n",*data_to_read);
 
 
 
-// //register to write 0x14
-// reg = 0x14;
-// data_to_write[1]=0x04;//msb
-// data_to_write[0]=0x00;//lsb
+//register to write 0x14
+reg = 0x14;
+data_to_write[1]=0x04;//msb
+data_to_write[0]=0x00;//lsb
 
-// // i2c_write_custom(slave_address, reg, data_to_write );
-// i2c_write_custom(slave_address, reg);
+// i2c_write_custom(slave_address, reg, data_to_write );
+i2c_write_custom(slave_address, reg,data_to_write);
 
-// //register to write 0x15
-// reg = 0x15;
-// data_to_write[1]=0x38;//msb
-// data_to_write[0]=0x00;//lsb
+//register to write 0x15
+reg = 0x15;
+data_to_write[1]=0x38;//msb
+data_to_write[0]=0x00;//lsb
 
-// // i2c_write_custom(slave_address, reg, data_to_write );
-// i2c_write_custom(slave_address, reg);
-
+// i2c_write_custom(slave_address, reg, data_to_write );
+i2c_write_custom(slave_address, reg,data_to_write);
 
 i2c_close();
+
 }
