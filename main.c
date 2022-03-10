662f1c18aaf4bfe2dcaae7921c1bd032f4babc87
bhagwat-99 <shindebhagavat030@gmail.com>
2022-03-10 18:05:13 +0530

    updated code check

--- main.c
+++ main.c
@@ -8,9 +8,31 @@ unsigned char reg;
 unsigned char data_to_write[2];
 unsigned char *data_to_read;
 
+
+void ManufacturingID()
+{
+        reg = 0xFE;
+        data_to_read = i2c_read(slave_address, reg);//reading result register
+        printf("Manufacturing ID %02x",*(data_to_read+1));
+        printf("%02x\n",*data_to_read);
+
+}
+
+void DeviceID()
+{
+        reg = 0xFF;
+        data_to_read = i2c_read(slave_address, reg);//reading result register
+        printf("Device ID %02x",*(data_to_read+1));
+        printf("%02x\n",*data_to_read);
+}
+
+
 int main()
 {
         i2c_init(i2c_bus);
+
+        ManufacturingID();
+        DeviceID();
 
         reg = 0x14;
         data_to_write[0] = 0x00;//lsb
@@ -23,15 +45,8 @@ int main()
         i2c_write(slave_address, reg,data_to_write);//writing configuration register
 
 
-        reg = 0xFE;
-        data_to_read = i2c_read(slave_address, reg);//reading result register
-        printf("Manufacturing ID %04x",*(data_to_read+1));
-        printf("%04x\n",*data_to_read);
-
-        reg = 0xFF;
-        data_to_read = i2c_read(slave_address, reg);//reading result register
-        printf("Device ID %04x",*(data_to_read+1));
-        printf("%04x\n",*data_to_read);
+        
+        
 
         
 
