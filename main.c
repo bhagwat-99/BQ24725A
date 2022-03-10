bfababd2bd6fa9f7817391e12ceba30711e229d3
bhagwat-99 <shindebhagavat030@gmail.com>
2022-03-10 11:50:37 +0530

    configure + read

--- main.c
+++ main.c
@@ -11,9 +11,8 @@ int main()
 i2c_init(i2c_bus);
 
 unsigned char* data_to_read;
-// unsigned char data_to_write[2];
-// data_to_write[0]=0x00;
-// data_to_write[0]=0x00;
+unsigned char data_to_write[2];
+
 
 
 
@@ -30,20 +29,22 @@ reg = 0xFF;
 data_to_read = i2c_read(slave_address, reg);
 
 printf("Device ID  %x",*(data_to_read+1));
-printf("%x",*data_to_read);
+printf("%x\n",*data_to_read);
 
 
-// //register to write 0x14
-// reg = 0x14;
-// data_to_write[0]=0x00;//lsb
-// data_to_write[1]=0x04;//msb
-// i2c_write_custom(&fd_i2c ,slave_address, reg, &data_to_write );
+//register to write 0x14
+reg = 0x14;
+data_to_write[1]=0x04;//msb
+data_to_write[0]=0x00;//lsb
 
-// //register to write 0x15
-// reg = 0x15;
-// data_to_write[0]=0x00;//lsb
-// data_to_write[1]=0x38;//msb
-// i2c_write_custom(&fd_i2c ,slave_address, reg, &data_to_write );
+i2c_write_custom(slave_address, reg, data_to_write );
+
+//register to write 0x15
+reg = 0x15;
+data_to_write[1]=0x38;//msb
+data_to_write[0]=0x00;//lsb
+
+i2c_write_custom(slave_address, reg, data_to_write );
 
 
 i2c_close();
