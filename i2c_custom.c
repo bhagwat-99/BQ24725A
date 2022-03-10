f5d9c2105e084e3a9ab46feef76d8ff0c4d0ea40
bhagwat-99 <shindebhagavat030@gmail.com>
2022-03-10 15:10:23 +0530

    code change

--- i2c_custom.c
+++ i2c_custom.c
@@ -1,86 +1,91 @@
-#include "i2c_custom.h"
 
-//file descriptor
-int fd_i2c = -1;
-unsigned char data_read[2];
 
-void i2c_init(char *i2c_bus)
-{
+
+
+
+// #include "i2c_custom.h"
+
+// //file descriptor
+// int fd_i2c = -1;
+// unsigned char data_read[2];
+
+// void i2c_init(char *i2c_bus)
+// {
    
-    if ((fd_i2c = open(i2c_bus, O_RDWR)) < 0)
-    {
-        printf("Failed to open apalis-i2c1.");
+//     if ((fd_i2c = open(i2c_bus, O_RDWR)) < 0)
+//     {
+//         printf("Failed to open apalis-i2c1.");
         
-    }
+//     }
 
-}
+// }
 
-void i2c_close() 
-{
-    close(fd_i2c);
-}
+// void i2c_close() 
+// {
+//     close(fd_i2c);
+// }
 
 
 
-unsigned char * i2c_read(unsigned char slave_addr, unsigned char reg) 
-{
-        unsigned char data_write[1];
+// unsigned char * i2c_read(unsigned char slave_addr, unsigned char reg) 
+// {
+//         unsigned char data_write[1];
         
 
-        data_write[0]= reg;
-        data_read[0] = 0;
-        data_read[1] = 0;
+//         data_write[0]= reg;
+//         data_read[0] = 0;
+//         data_read[1] = 0;
 
 
-        struct i2c_msg msgs[2];
-        struct i2c_rdwr_ioctl_data msgset[1];
+//         struct i2c_msg msgs[2];
+//         struct i2c_rdwr_ioctl_data msgset[1];
 
-        msgs[0].addr = slave_addr;
-        msgs[0].flags = 0;
-        msgs[0].len = 1;
-        msgs[0].buf = data_write;
+//         msgs[0].addr = slave_addr;
+//         msgs[0].flags = 0;
+//         msgs[0].len = 1;
+//         msgs[0].buf = data_write;
 
-        msgs[1].addr = slave_addr;
-        msgs[1].flags = I2C_M_RD;
-        msgs[1].len = 2;
-        msgs[1].buf = data_read;
+//         msgs[1].addr = slave_addr;
+//         msgs[1].flags = I2C_M_RD;
+//         msgs[1].len = 2;
+//         msgs[1].buf = data_read;
 
-        msgset[0].msgs = msgs;
-        msgset[0].nmsgs = 2;
+//         msgset[0].msgs = msgs;
+//         msgset[0].nmsgs = 2;
 
-        if(ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
-                perror("ioctl(I2C_RDWR) in i2c_read");
+//         if(ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
+//                 perror("ioctl(I2C_RDWR) in i2c_read");
                 
-        }
+//         }
         
-        return data_read;
-}
+//         return data_read;
+// }
 
-void i2c_write_custom(unsigned char slave_addr, unsigned char reg, unsigned char *data_to_write )
-{
-    struct i2c_msg msgs[1];
-    struct i2c_rdwr_ioctl_data msgset[1];
+// void i2c_write_custom(unsigned char slave_addr, unsigned char reg, unsigned char *data_to_write )
+// {
+//     struct i2c_msg msgs[1];
+//     struct i2c_rdwr_ioctl_data msgset[1];
 
-    unsigned char data_write[3];
+//     unsigned char data_write[3];
 
-    data_write[0] = reg;
-    data_write[1] = *data_to_write;
-    data_write[2] = *(data_to_write+1);
+//     data_write[0] = reg;
+//     data_write[1] = *data_to_write;
+//     data_write[2] = *(data_to_write+1);
 
-    printf("data_write[2] %x\n",data_write[2]);
-    printf("data_write[1] %x\n",data_write[1]);
+//     printf("data_write[2] %x\n",data_write[2]);
+//     printf("data_write[1] %x\n",data_write[1]);
 
 
-    msgs[0].addr = slave_addr;
-    msgs[0].flags = 0;// 0 for write 
-    msgs[0].len = 3;
-    msgs[0].buf = data_write;
+//     msgs[0].addr = slave_addr;
+//     msgs[0].flags = 0;// 0 for write 
+//     msgs[0].len = 3;
+//     msgs[0].buf = data_write;
 
-    msgset[0].msgs = msgs;
-    msgset[0].nmsgs = 1;
+//     msgset[0].msgs = msgs;
+//     msgset[0].nmsgs = 1;
 
-    if(ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
-        perror("ioctl(I2C_RDWR) in i2c_write");
-    }
+//     if(ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
+//         perror("ioctl(I2C_RDWR) in i2c_write");
+//     }
 
-}
+// }
--- main.c
+++ main.c
@@ -1,51 +1,173 @@
-#include "i2c_custom.h"
 
-//slave address
+#include <stdio.h> // printf()
+#include <sys/types.h> // open()
+#include <sys/stat.h> // open()
+#include <fcntl.h> // open()
+#include <sys/ioctl.h> // ioctl()
+#include <errno.h> // errno
+#include <string.h> // strerror()
+#include <unistd.h> // close()
+#include <linux/i2c-dev.h> // struct i2c_msg
+#include <linux/i2c.h> // struct i2c_rdwr_ioctl_data
+
+int fd_i2c = -1; // i2c bus file descriptor
+const char *i2c_bus = "/dev/apalis-i2c1";
 unsigned char slave_address = 0x09;
-char *i2c_bus =  "/dev/apalis-i2c1";
 
+
+int i2c_init(void)
+{
+    if ((fd_i2c = open(i2c_bus, O_RDWR)) < 0)
+    {
+        printf("Failed to open apalis-i2c1.");
+        return -1;
+    }
+    return fd_i2c;
+}
+
+void i2c_close(void) 
+{
+    close(fd_i2c);
+}
+
+// Write to an I2C slave device's register:
+int i2c_write(unsigned char slave_addr, unsigned char reg, unsigned char low_byte, unsigned char high_byte )
+{
+    struct i2c_msg msgs[1];
+    struct i2c_rdwr_ioctl_data msgset[1];
+
+    unsigned char outbuf[3];
+
+    outbuf[0] = reg;
+    outbuf[1] = low_byte;
+    outbuf[2] = high_byte;
+
+    msgs[0].addr = slave_addr;
+    msgs[0].flags = 0;// 0 for write 
+    msgs[0].len = 3;
+    msgs[0].buf = outbuf;
+
+    msgset[0].msgs = msgs;
+    msgset[0].nmsgs = 1;
+
+    if (ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
+        perror("ioctl(I2C_RDWR) in i2c_write");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+// Read the given I2C slave device's register and return the read value in `*result`:
+__uint16_t i2c_read(unsigned char slave_addr, unsigned char reg) 
+{
+    unsigned char outbuf[1];
+    unsigned char inbuf[2];
+
+    outbuf[0]=reg;
+    inbuf[0] = 0;
+    inbuf[1] = 0;
+
+
+    struct i2c_msg msgs[2];
+    struct i2c_rdwr_ioctl_data msgset[1];
+
+    msgs[0].addr = slave_addr;
+    msgs[0].flags = 0;
+    msgs[0].len = 1;
+    msgs[0].buf = outbuf;
+
+    msgs[1].addr = slave_addr;
+    msgs[1].flags = I2C_M_RD;
+    msgs[1].len = 2;
+    msgs[1].buf = inbuf;
+
+    msgset[0].msgs = msgs;
+    msgset[0].nmsgs = 2;
+
+    if (ioctl(fd_i2c, I2C_RDWR, &msgset) < 0) {
+        perror("ioctl(I2C_RDWR) in i2c_read");
+        return -1;
+    }
+        printf("inbuf[0] %x\n",inbuf[0]);
+        printf("inbuf[1] %x\n",inbuf[1]);
+    __uint16_t reg_value = inbuf[1]*256 + inbuf[0] ;
+    return reg_value;
+}
 
 int main()
 {
+    i2c_init();
 
-i2c_init(i2c_bus);
-
-unsigned char* data_to_read;
-unsigned char data_to_write[2];
-
+        i2c_write(0x09, 0x14, 0x00, 0x04 );//writing configuration register
+        i2c_write(0x09, 0x15, 0x00, 0x38 );//writing configuration register
 
 
+   // sleep(1);
 
-//read manufacturing id at 0xFEH
-unsigned char reg = 0xFE;
-data_to_read = i2c_read(slave_address, reg);
-printf("Manufacturing ID     %x",*(data_to_read+1));
-printf("%x\n",*data_to_read);
+    i2c_read(slave_address, 0x14);//reading result register
 
-//read device id at 0xFFH
-reg = 0xFF;
-data_to_read = i2c_read(slave_address, reg);
-printf("Device ID  %x",*(data_to_read+1));
-printf("%x\n",*data_to_read);
+    i2c_read(slave_address, 0x15);//reading result register
 
+    //float Lux = reg_value * 0.0576 ; // gain = 1 and integration time = 100ms multiplication factor = 0.0576
 
-
-//register to write 0x14
-reg = 0x14;
-data_to_write[1]=0x04;//msb
-data_to_write[0]=0x00;//lsb
-
-// i2c_write_custom(slave_address, reg, data_to_write );
-i2c_write_custom(slave_address, reg,data_to_write);
-
-//register to write 0x15
-reg = 0x15;
-data_to_write[1]=0x38;//msb
-data_to_write[0]=0x00;//lsb
-
-// i2c_write_custom(slave_address, reg, data_to_write );
-i2c_write_custom(slave_address, reg,data_to_write);
-
-i2c_close();
-
+   // printf("Light Intensity : %.2f Lux \n",Lux);
+    i2c_close();
+    return 0;
 }
+
+
+
+
+// #include "i2c_custom.h"
+
+// //slave address
+// unsigned char slave_address = 0x09;
+// char *i2c_bus =  "/dev/apalis-i2c1";
+
+
+// int main()
+// {
+
+// i2c_init(i2c_bus);
+
+// unsigned char* data_to_read;
+// unsigned char data_to_write[2];
+
+
+
+
+// //read manufacturing id at 0xFEH
+// unsigned char reg = 0xFE;
+// data_to_read = i2c_read(slave_address, reg);
+// printf("Manufacturing ID     %x",*(data_to_read+1));
+// printf("%x\n",*data_to_read);
+
+// //read device id at 0xFFH
+// reg = 0xFF;
+// data_to_read = i2c_read(slave_address, reg);
+// printf("Device ID  %x",*(data_to_read+1));
+// printf("%x\n",*data_to_read);
+
+
+
+// //register to write 0x14
+// reg = 0x14;
+// data_to_write[1]=0x04;//msb
+// data_to_write[0]=0x00;//lsb
+
+// // i2c_write_custom(slave_address, reg, data_to_write );
+// i2c_write_custom(slave_address, reg,data_to_write);
+
+// //register to write 0x15
+// reg = 0x15;
+// data_to_write[1]=0x38;//msb
+// data_to_write[0]=0x00;//lsb
+
+// // i2c_write_custom(slave_address, reg, data_to_write );
+// i2c_write_custom(slave_address, reg,data_to_write);
+
+// i2c_close();
+
+// }
